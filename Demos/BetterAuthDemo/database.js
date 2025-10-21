9988b8b47bb415688d596492d24f1a0e0a2c1566
Michael-Rhoades <Michael_Rhoades@insideranken.org>
2025-10-21 16:22:29 -0500

    Start of testing Better Auth

---
+++ Demos/BetterAuthDemo/database.js
@@ -0,0 +1,40 @@
+import { MongoClient, ObjectId } from "mongodb";
+import debug from "debug";
+const debugDb = debug("app:database");
+
+let _db = null;
+let _client = null;
+
+async function connectToDatabase() {
+  if (!_db) {
+   // debugDb(`Connecting to database...1`);
+    const connectionString = process.env.MONGO_URI;
+    const dbName = process.env.MONGO_DB_NAME;
+    
+    _client = await MongoClient.connect(connectionString);
+    
+    _db = _client.db(dbName);
+  }
+  return _db;
+}
+
+async function ping() {
+ 
+  const db = await connectToDatabase();
+  const pong = await db.command({ ping: 1 });
+  debugDb("Ping", pong);
+}
+
+async function getClient() {
+  if (!_client) {
+    await connectToDatabase();
+  }
+  return _client;
+}
+
+async function getDatabase() {
+  return await connectToDatabase();
+}
+
+
+export { getClient, getDatabase };
---
+++ Demos/BetterAuthDemo/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { toNodeHandler } from 'better-auth/node';
+import auth from './auth.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.all('/api/auth/"splat"', toNodeHandler(auth));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
